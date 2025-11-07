de719bf23ffa43c215b82c889861266b2df03342
Romel Lachica <romeldiazlahica@gmail.com>
2025-11-07 22:26:59 +0800

    Force admin creation on Render startup

--- app.py
+++ app.py
@@ -198,5 +198,7 @@ def init_admin():
         else:
             print("ℹ️ Admin already exists:", admin_email)
 
+init_admin()
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)), debug=True)
