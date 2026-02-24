fc3ac73b851781ab6e3f73552d0a27904dcbc3e9
Romel Lachica <romeldiazlahica@gmail.com>
2026-02-25 02:24:50 +0800

    try2

--- app.py
+++ app.py
@@ -1540,5 +1540,6 @@ def export_csv():
     output.seek(0)
     return send_file(output, mimetype='text/csv', as_attachment=True, download_name='dogs.csv')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)), debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
